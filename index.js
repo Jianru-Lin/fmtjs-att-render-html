93bd4a3470ebdf75058b9781d5f9c0c4fdf09f19
Jianru Lin <jianru.lin@gmail.com>
2016-08-19 17:39:01 +0800

    BUG FIX

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var assets = require('./lib/assets')
 var html_render = require('./lib/html_render')
 
-exports.render = function(att, options) {
+module.exports = render
+
+function render(att, options) {
 	var filename = options.filename || 'unknown'
 	var html_content_str = html_render(att, options)
 	var result = assets.load().fill(html_content_str, filename)
