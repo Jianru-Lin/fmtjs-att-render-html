30ddfd6fd71555bcfc43a6dc8e8284e625390b56
Jianru Lin <jianru.lin@gmail.com>
2016-08-19 16:09:46 +0800

    基本的机构已经成型

---
+++ index.js
@@ -0,0 +1,11 @@
+var assets = require('./lib/assets')
+var html_render = require('./lib/html_render')
+
+exports.render = function(att, options) {
+	var filename = options.filename || 'unknown'
+	var html_content_str = html_render(att, options)
+	var result = assets.load().fill(html_content_str, filename)
+	return result
+}
+
+console.log(exports.render({}, {filename: 'my.js'}))
