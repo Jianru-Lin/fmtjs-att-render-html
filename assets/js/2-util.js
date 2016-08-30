edeef9163a2bd89fa724906d481821e10f51f512
Jianru Lin <jianru.lin@gmail.com>
2016-08-30 15:35:38 +0800

    关闭了解析时的日志输出

--- assets/js/2-util.js
+++ assets/js/2-util.js
@@ -1,6 +1,9 @@
 function log(type, text) {
+	if (!log.enabled) return
 	var div = document.createElement('div')
 	div.setAttribute('class', type)
 	div.textContent = '[?1] ?2'.replace('?1', type).replace('?2', text)
 	$('#log').append(div)
 }
+
+log.enabled = false
