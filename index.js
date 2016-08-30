7fb031a1a59a73935d5dc610b9aba5aa956ec4d7
Jianru Lin <jianru.lin@gmail.com>
2016-08-31 00:29:57 +0800

    改用 jade 来来生 html

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+var jade = require('jade')
 var assets = require('./lib/assets')
-var html_render = require('./lib/html_render')
 
 module.exports = render
 
 function render(att, options) {
 	var filename = options.filename || 'unknown'
-	var html_content_str = html_render(att, options)
-	var result = assets.load().fill(html_content_str, filename)
-	return result
+	var a = assets.load()
+	var fn = jade.compile(a.template, {})
+	return fn({
+		filename: filename,
+		style: a.css,
+		script: a.js,
+		init: gen_init_code(att)
+	})
+
+	function gen_init_code(att) {
+		var code = 'window.ast=JSON.parse(Base64.decode("?"));'.replace('?', base64(json(att)))
+		return code
+
+		function json(obj) {
+			return JSON.stringify(obj)
+		}
+
+		function base64(text) {
+			return new Buffer(text).toString('base64')
+		}
+	}
 }
