4b5ba22b177c851beb8ee418aec6255d864e92fd
Jianru Lin <jianru.lin@gmail.com>
2017-01-02 00:26:32 +0800

    重构

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 var pkg = require('./package.json')
-var pug = require('pug')
-var assets = require('./lib/assets')
+var path = require('path')
+var fs = require('fs')
+var web = require('fmtjs-web')
+var web_cache = require('fmtjs-web/cache')
 
 module.exports = render
 
 function render(att, options) {
+	var id = hash(JSON.stringify(att))
 	var filename = options.filename || 'unknown'
 	var version = options.version || pkg.version
-	var a = assets.load()
-	var fn = pug.compile(a.template, {})
-	return fn({
+	// generate index data
+	index_data(version, id, filename, att).save()
+	// ok, return dummy html content
+	var html_path = web.lv_url(id)
+	return '<script>location.href="' + html_path + '"</script>'
+}
+
+function index_data(version, id, filename, att) {
+	return {
+		id: id,
 		filename: filename,
 		version: version,
-		style: a.css,
-		script: a.js,
-		init: gen_init_code(att)
-	})
-
-	function gen_init_code(att) {
-		var code = 'window.att=JSON.parse(Base64.decode("?"));'.replace('?', base64(json(att)))
-		return code
-
-		function json(obj) {
-			return JSON.stringify(obj)
-		}
-
-		function base64(text) {
-			return new Buffer(text).toString('base64')
+		att: att,
+		save: function() {
+			web_cache.add(id, this)
 		}
 	}
+}
+
+function hash(text) {
+	var algorithm = require('crypto').createHash('sha256')
+	algorithm.update(text)
+	return algorithm.digest('hex')
 }
