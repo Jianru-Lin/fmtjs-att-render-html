5e22811c1a27abb49530a2389b47ba03ff61fe2b
Jianru Lin <jianru.lin@gmail.com>
2017-01-08 11:20:24 +0800

    update-version

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
+var assert = require('assert')
 var pkg = require('./package.json')
-var path = require('path')
-var fs = require('fs')
-var web = require('fmtjs-web')
-var web_cache = require('fmtjs-web/cache')
-
-// start web server in background
-web.start_bg()
+var dmgr = require('./dmgr')
 
 module.exports = render
 
 function render(att, options) {
-	var id = hash(JSON.stringify(att))
-	var filename = options.filename || 'unknown'
-	var version = options.version || pkg.version
 	// generate index data
-	index_data(version, id, filename, att).save()
-	// ok, return dummy html content
-	var html_path = web.lv_url(id)
-	return '<script>location.href="' + html_path + '"</script>'
-}
-
-function index_data(version, id, filename, att) {
-	return {
-		id: id,
-		filename: filename,
-		version: version,
-		att: att,
-		save: function() {
-			web_cache.add(id, this)
-		}
-	}
-}
-
-function hash(text) {
-	var algorithm = require('crypto').createHash('sha256')
-	algorithm.update(text)
-	return algorithm.digest('hex')
+	var url = dmgr.save({
+		filename: options.filename || 'unknown',
+		version: options.version || pkg.version,
+		att: att
+	})
+	assert(typeof url === 'string')
+	assert(/^http(s)?:\/\//.test(url))
+	return '<script>location.href=' + JSON.stringify(url) + '</script>'
 }
